441fe11a7a41bc0cc54909d43866d11a0d9d49a3
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2019-03-08 14:09:53 -0600

    move show method

--- R/Shadow_class.R
+++ R/Shadow_class.R
@@ -226,6 +226,66 @@ setClass("Shadow.config",
          }
 )
 
+#' @docType methods
+#' @rdname show-methods
+#' @export
+setMethod("show", "Shadow.config", function(object) {
+  cat("Shadow Configuration Settings \n\n")
+  cat("  Item selection criterion \n")
+  cat("    Method         :", object@itemSelection$method, "\n")
+  cat("    Info type      :", object@itemSelection$infoType, "\n")
+  cat("    Initial theta  :", object@itemSelection$initialTheta, "\n\n")
+  cat("  Content balancing \n")
+  cat("    Method         :", object@contentBalancing$method, "\n\n")
+  cat("  MIP \n")
+  cat("    Solver         :", object@MIP$solver, "\n")
+  cat("    Verbosity      :", object@MIP$verbosity, "\n")
+  cat("    Time limit     :", object@MIP$timeLimit, "\n")
+  cat("    Gap limit      :", object@MIP$gapLimit, "\n\n")
+  cat("  MCMC \n")
+  cat("    Burn in        :", object@MCMC$burnIn, "\n")
+  cat("    Post burn in   :", object@MCMC$postBurnIn, "\n")
+  cat("    Thin           :", object@MCMC$thin, "\n")
+  cat("    Jump factor    :", object@MCMC$jumpFactor, "\n\n")
+  cat("  Refresh policy \n")
+  cat("    Method         :", object@refreshPolicy$method, "\n")
+  cat("    Interval       :", object@refreshPolicy$interval, "\n")
+  cat("    Threshold      :", object@refreshPolicy$threshold, "\n")
+  cat("    Position       :", object@refreshPolicy$position, "\n\n")
+  cat("  Exposure control \n")
+  cat("    Method         :", object@exposureControl$method, "\n")
+  cat("    Big M          :", object@exposureControl$M, "\n")
+  cat("    Max Exposure   :", object@exposureControl$maxExposureRate, "\n")
+  cat("    N Segment      :", object@exposureControl$nSegment, "\n")
+  cat("    Segment cut    :", object@exposureControl$segmentCut, "\n")
+  cat("    Fading factor  :", object@exposureControl$fadingFactor, "\n")
+  cat("    Acceleration   :", object@exposureControl$accelerationFactor, "\n")
+  cat("    Diagnostics    :", object@exposureControl$diagnosticStats, "\n\n")
+  cat("  Stopping criterion \n")
+  cat("    Method         :", object@stoppingCriterion$method, "\n")
+  if (toupper(object@stoppingCriterion$method) == "FIXED") {
+    cat("    Test Length    :", object@stoppingCriterion$testLength, "\n")
+  } else {
+    cat("    Min # of items :", object@stoppingCriterion$minNI, "\n")
+    cat("    Max # of items :", object@stoppingCriterion$maxNI, "\n")
+    cat("    SE threshold   :", object@stoppingCriterion$SeThreshold, "\n")
+  }
+  cat("    Min # of items :", object@stoppingCriterion$minNI, "\n")
+  cat("    Max # of items :", object@stoppingCriterion$maxNI, "\n")
+  cat("    SE threshold   :", ifelse(toupper(object@stoppingCriterion$method) == "VARIABLE", object@stoppingCriterion$SeThreshold, NA), "\n\n")
+  cat("  Interim theta estimator \n")
+  cat("    Method         :", object@interimTheta$method, "\n")
+  cat("    Prior dist     :", ifelse(toupper(object@interimTheta$method == "EAP"), object@interimTheta$priorDist, NA), "\n")
+  cat("    Prior parm     :", ifelse(toupper(object@interimTheta$method == "EAP"), sprintf(ifelse(toupper(object@interimTheta$priorDist) == "NORMAL", "Mean = %5.3f, SD = %5.3f", "Min = %5.3f, Max = %5.3f"), object@interimTheta$priorPar[1], object@interimTheta$priorPar[2]), NA), "\n\n")
+  cat("  Final theta estimator \n")
+  cat("    Method         :", object@finalTheta$method, "\n")
+  cat("    Prior dist     :", ifelse(toupper(object@finalTheta$method == "EAP"), object@finalTheta$priorDist, NA), "\n")
+  cat("    Prior parm     :", ifelse(toupper(object@finalTheta$method == "EAP"), sprintf(ifelse(toupper(object@finalTheta$priorDist) == "NORMAL", "Mean = %5.3f, SD = %5.3f", "Min = %5.3f, Max = %5.3f"), object@finalTheta$priorPar[1], object@finalTheta$priorPar[2]), NA), "\n\n")
+  cat("  Theta Grid \n")
+  print(object@thetaGrid)
+  cat("\n  Plot Audit Trail: ", object@auditTrail, "\n")
+})
+
 #' Shadow.output
 #'
 #' @slot simuleeIndex Numeric.
@@ -299,3 +359,26 @@ setClass("Shadow.output",
            return(TRUE)
          }
 )
+
+#' @docType methods
+#' @rdname show-methods
+#' @export
+setMethod("show", "Shadow.output", function(object) {
+  if (length(object@administeredItemIndex) > 0) {
+    cat("Simulee Index          :", object@simuleeIndex, "\n")
+    cat("  True Theta           :", object@trueTheta, "\n")
+    cat("  Final Theta Estimate :", object@finalThetaEst, "\n")
+    cat("  Final SE Estimate    :", object@finalSeEst, "\n")
+    output = data.frame(Stage = 1:length(object@administeredItemIndex),
+                        StimulusIndex = ifelse(is.nan(object@administeredStimulusIndex), rep(NA, length(object@administeredItemIndex)), object@administeredStimulusIndex),
+                        ItemIndex = object@administeredItemIndex,
+                        ItemResp = object@administeredItemResp,
+                        InterimTheta = object@interimThetaEst,
+                        InterimSE = object@interimSeEst,
+                        ThetaSegment = object@thetaSegmentIndex)
+    print(output)
+  } else {
+    cat("empty object of class Shadow.output\n")
+  }
+  cat("\n")
+})
