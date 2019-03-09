f2c689023b44aac36c4c687ca9aed926b7c398ab
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2019-03-09 09:27:05 -0600

    add Shiny app

---
+++ R/runshiny.R
@@ -0,0 +1,40 @@
+#' app
+#'
+#' Launches a Shiny app locally.
+#'
+#' @export
+app <- function() {
+  appDir <- system.file("shiny", package = "Shadow")
+  if (appDir == "") {
+    stop("Could not find application directory. Try re-installing `Shadow`.", call. = FALSE)
+  }
+
+  tmp = NULL
+  if (!requireNamespace("shiny")) tmp = c(tmp, "'shiny'")
+  if (!requireNamespace("shinythemes")) tmp = c(tmp, "'shinythemes'")
+  if (!requireNamespace("shinyWidgets")) tmp = c(tmp, "'shinyWidgets'")
+  if (!requireNamespace("shinyjs")) tmp = c(tmp, "'shinyjs'")
+  if (!requireNamespace("DT")) tmp = c(tmp, "'DT'")
+  if (!requireNamespace("readxl")) tmp = c(tmp, "'readxl'")
+
+  if (!is.null(tmp)){
+    tmp = paste(tmp, collapse = ", ")
+    message("Shiny application requires additional packages.")
+    message("Run the following code to install:")
+    message("")
+    tmp = paste0("install.packages(", tmp, ")")
+    message(tmp)
+  } else {
+    shiny::runApp(appDir, display.mode = "normal")
+  }
+}
+
+#' guiShadow
+#'
+#' Launches a Shiny app locally.
+#'
+#' @export
+
+guiShadow <- function() {
+  app()
+}
