f20d7f9a88104b6e76ff464cf2519b5898ae3b11
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2019-03-20 18:11:03 -0500

    add documentation

--- R/datasets.R
+++ R/datasets.R
@@ -1,170 +1,228 @@
 # Documentation progress
 # Phase 1: Create a skeleton structure    -- complete
 
-#' attrib_pool_A
+#' item_params_A
 #'
-#' A pretty dataset you won't find anywhere else
+#' Item-based pool A (n = 300).
 #'
+#' This pool is associated with the following objects:
 #' \itemize{
-#'   \item var. A nice variable
+#'   \item \code{item_params_A} Item parameters.
+#'   \item \code{item_se_A} Standard errors of the item parameters.
+#'   \item \code{item_attribs_A} Item attributes.
+#'   \item \code{constraints_0} A simple constraint set (1 constraint).
+#'   \item \code{constraints_1} A complex constraint set (14 constraints).
 #' }
 #'
 #' @docType data
 #' @keywords datasets
-#' @name attrib_pool_A
-#' @format A neat format
+#' @rdname dataset_A
+#' @name item_params_A
 NULL
 
-#' attrib_pool_B
-#'
-#' A pretty dataset you won't find anywhere else
-#'
-#' \itemize{
-#'   \item var. A nice variable
-#' }
+#' item_se_A
 #'
 #' @docType data
 #' @keywords datasets
-#' @name attrib_pool_B
-#' @format A neat format
+#' @rdname dataset_A
+#' @name item_se_A
 NULL
 
-#' attrib_pool_C
+#' item_attribs_A
 #'
-#' A pretty dataset you won't find anywhere else
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_A
+#' @name item_attribs_A
+NULL
+
+
+
+#' item_params_B
 #'
+#' Item-based pool B (n = 300).
+#'
+#' This pool is associated with the following objects:
 #' \itemize{
-#'   \item var. A nice variable
+#'   \item \code{item_params_B} Item parameters.
+#'   \item \code{item_se_B} Standard errors of the item parameters.
+#'   \item \code{item_attribs_B} Item attributes.
+#'   \item \code{constraints_0} A simple constraint set (1 constraint).
+#'   \item \code{constraints_1} A complex constraint set (14 constraints).
 #' }
 #'
 #' @docType data
 #' @keywords datasets
-#' @name attrib_pool_C
-#' @format A neat format
+#' @rdname dataset_B
+#' @name item_params_B
 NULL
+
+#' item_se_B
+#'
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_B
+#' @name item_se_B
+NULL
+
+#' item_attribs_B
+#'
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_B
+#' @name item_attribs_B
+NULL
+
+
+#' item_params_C
+#'
+#' Item-based pool C (n = 300).
+#'
+#' This pool is associated with the following objects:
+#' \itemize{
+#'   \item \code{item_params_C} Item parameters.
+#'   \item \code{item_se_C} Standard errors of the item parameters.
+#'   \item \code{item_attribs_C} Item attributes.
+#'   \item \code{constraints_0} A simple constraint set (1 constraint).
+#'   \item \code{constraints_1} A complex constraint set (14 constraints).
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_C
+#' @name item_params_C
+NULL
+
+#' item_se_C
+#'
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_C
+#' @name item_se_C
+NULL
+
+#' item_attribs_C
+#'
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_C
+#' @name item_attribs_C
+NULL
+
 
 #' constraints_0
 #'
-#' A pretty dataset you won't find anywhere else
-#'
-#' \itemize{
-#'   \item var. A nice variable
-#' }
+#' A simple constraint set (1 constraint) to use with \code{item_params_A, item_params_B, item_params_C, item_attribs_A, item_attribs_B, item_attribs_C}.
 #'
 #' @docType data
 #' @keywords datasets
 #' @name constraints_0
-#' @format A neat format
+#' @format A data.frame.
 NULL
 
 #' constraints_1
 #'
-#' A pretty dataset you won't find anywhere else
-#'
-#' \itemize{
-#'   \item var. A nice variable
-#' }
+#' A more complex constraint set (14 constraints) to use with \code{item_params_A, item_params_B, item_params_C, item_attribs_A, item_attribs_B, item_attribs_C}.
 #'
 #' @docType data
 #' @keywords datasets
 #' @name constraints_1
-#' @format A neat format
+#' @format A data.frame.
 NULL
 
-#' par_mixed
+
+#' item_params_stimbased
 #'
-#' A pretty dataset you won't find anywhere else
+#' Stimulus-based pool.
 #'
+#' This pool is associated with the following objects:
 #' \itemize{
-#'   \item var. A nice variable
+#'   \item \code{item_params_stimbased} Item parameters.
+#'   \item \code{item_attribs_stimbased} Item attributes.
+#'   \item \code{stim_attribs_stimbased} Stimulus attributes.
+#'   \item \code{constraints_stimbased} A constraint set.
 #' }
 #'
 #' @docType data
 #' @keywords datasets
-#' @name par_mixed
-#' @format A neat format
+#' @rdname dataset_stimbased
+#' @name item_params_stimbased
 NULL
 
-#' par_Pool_A
+#' item_attribs_stimbased
 #'
-#' A pretty dataset you won't find anywhere else
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_stimbased
+#' @name item_attribs_stimbased
+NULL
+
+#' stim_attribs_stimbased
 #'
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_stimbased
+#' @name stim_attribs_stimbased
+NULL
+
+#' constraints_stimbased
+#'
+#' @docType data
+#' @keywords datasets
+#' @rdname dataset_stimbased
+#' @name constraints_stimbased
+NULL
+
+
+
+
+#' item_params_fatigue
+#'
+#' Item-based pool with item contents.
+#'
+#' This pool is associated with the following objects:
 #' \itemize{
-#'   \item var. A nice variable
+#'   \item \code{item_params_fatigue} Item parameters.
+#'   \item \code{item_attribs_fatigue} Item attributes.
+#'   \item \code{item_contents_fatigue} Item contents.
+#'   \item \code{constraints_fatigue} A constraint set.
+#'   \item \code{raw_fatigue} Raw response data.
 #' }
 #'
 #' @docType data
 #' @keywords datasets
-#' @name par_Pool_A
-#' @format A neat format
+#' @rdname dataset_fatigue
+#' @name item_params_fatigue
 NULL
 
-#' par_Pool_B
-#'
-#' A pretty dataset you won't find anywhere else
-#'
-#' \itemize{
-#'   \item var. A nice variable
-#' }
+#' item_attribs_fatigue
 #'
 #' @docType data
 #' @keywords datasets
-#' @name par_Pool_B
-#' @format A neat format
+#' @rdname dataset_fatigue
+#' @name item_attribs_fatigue
 NULL
 
-#' par_Pool_C
-#'
-#' A pretty dataset you won't find anywhere else
-#'
-#' \itemize{
-#'   \item var. A nice variable
-#' }
+#' item_contents_fatigue
 #'
 #' @docType data
 #' @keywords datasets
-#' @name par_Pool_C
-#' @format A neat format
+#' @rdname dataset_fatigue
+#' @name item_contents_fatigue
 NULL
 
-#' se_Pool_A
-#'
-#' A pretty dataset you won't find anywhere else
-#'
-#' \itemize{
-#'   \item var. A nice variable
-#' }
+#' constraints_fatigue
 #'
 #' @docType data
 #' @keywords datasets
-#' @name se_Pool_A
-#' @format A neat format
+#' @rdname dataset_fatigue
+#' @name constraints_fatigue
 NULL
 
-#' se_Pool_B
-#'
-#' A pretty dataset you won't find anywhere else
-#'
-#' \itemize{
-#'   \item var. A nice variable
-#' }
+#' raw_fatigue
 #'
 #' @docType data
 #' @keywords datasets
-#' @name se_Pool_B
-#' @format A neat format
-NULL
-
-#' se_Pool_C
-#'
-#' A pretty dataset you won't find anywhere else
-#'
-#' \itemize{
-#'   \item var. A nice variable
-#' }
-#'
-#' @docType data
-#' @keywords datasets
-#' @name se_Pool_C
-#' @format A neat format
+#' @rdname dataset_fatigue
+#' @name raw_fatigue
 NULL
