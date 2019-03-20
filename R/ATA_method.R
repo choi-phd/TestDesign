19291576429888942a80a0d1088fbeab9b362847
choi-phd <44212658+choi-phd@users.noreply.github.com>
2019-03-20 14:00:57 -0500

    make plot = FALSE default in ATA

--- R/ATA_method.R
+++ R/ATA_method.R
@@ -13,7 +13,7 @@
 #' @rdname ATA-methods
 
 setGeneric(name = "ATA",
-           def = function(config, Constraints, plot, plotrange = c(-3, 3)) {
+           def = function(config, Constraints, plot = FALSE, plotrange = c(-3, 3)) {
              standardGeneric("ATA")
            }
 )
@@ -29,11 +29,11 @@ setGeneric(name = "ATA",
 #' 
 #' @return A list containing the following entries:
 #' \itemize{
-#'   \item{MIP} Foo.
-#'   \item{Selected} Foo.
-#'   \item{solver} Foo.
-#'   \item{obj.value} Foo.
-#'   \item{solve.time} Foo.
+#'   \item{MIP} MIP model.
+#'   \item{Selected} Solution returned.
+#'   \item{solver} MIP solver.
+#'   \item{obj.value} Objective value returned.
+#'   \item{solve.time} Solver elapsed time.
 #' }
 #'
 #' @export
@@ -46,7 +46,7 @@ setGeneric(name = "ATA",
 
 setMethod(f = "ATA",
           signature = c("ATA.config"),
-          definition = function(config, Constraints, plot, plotrange = c(-3, 3)) {
+          definition = function(config, Constraints, plot = FALSE, plotrange = c(-3, 3)) {
             if (!validObject(config)) {
               stop("invalid configuration options specified")
             }
@@ -202,6 +202,8 @@ setMethod(f = "ATA",
               p = recordPlot()
               plot.new()
               dev.off()
+            } else {
+              p = NULL
             }
             
             return(list(MIP = MIP, Selected = Selected, solver = config@MIP$solver, obj.value = obj.value, solve.time = solve.time, plot = p))
