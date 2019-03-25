945cd87d108b47f1204d5a02debae824892e5dd2
Seung Choi <schoi@austin.utexas.edu>
2019-03-25 16:05:57 -0500

    Update Shadow_functions.R
    apply penality to alpha_ijk for items (administered in other segments) that are ineligible in the final segment

--- R/Shadow_functions.R
+++ R/Shadow_functions.R
@@ -1752,7 +1752,7 @@ setMethod(f = "Shadow",
                           }
                         }
                         
-                        optimal = STA(Constraints, info, xmat = rbind(xmat, imat), xdir = c(xdir, idir), xrhs = c(xrhs, irhs), maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit)
+                        optimal = STA(Constraints, info, xmat = rbind(xmat, imat), xdir = c(xdir, idir), xrhs = c(xrhs, irhs), maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit, solver = config@MIP$solver)
                         
                         #nrow(optimal$shadowTest) can be less than testLength when names(optimal$status) == "PREP_OPTIMAL_SOLUTION_FOUND" - this causes the program to stop
                         if (!names(optimal$status) %in% c("TM_OPTIMAL_SOLUTION_FOUND", "PREP_OPTIMAL_SOLUTION_FOUND")) {
@@ -1760,7 +1760,7 @@ setMethod(f = "Shadow",
                           #if infeasible - "PREP_NO_SOLUTION"
                           output@shadowTestFeasible[position] = FALSE #superflous; initialized to FALSE
                           #remove all ineligibility constraints
-                          optimal = STA(Constraints, info, xmat = imat, xdir = idir, xrhs = irhs, maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit)
+                          optimal = STA(Constraints, info, xmat = imat, xdir = idir, xrhs = irhs, maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit, solver = config@MIP$solver)
                           #optimal = STA(Constraints, info, xmat = NULL, xdir = NULL, xrhs = NULL, maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit)
                         } else {
                           output@shadowTestFeasible[position] = TRUE
@@ -1774,13 +1774,13 @@ setMethod(f = "Shadow",
                           info[itemIneligible == 1] = -1 * maxInfo - 1
                         }
                         
-                        optimal = STA(Constraints, info, xmat = imat, xdir = idir, xrhs = irhs, maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit)
+                        optimal = STA(Constraints, info, xmat = imat, xdir = idir, xrhs = irhs, maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit, solver = config@MIP$solver)
                         output@shadowTestFeasible[position] = TRUE
                       } 
                       #end itemIneligibilityControl
                     } else {
                       #if not itemEligibilityControl
-                      optimal = STA(Constraints, info, xmat = imat, xdir = idir, xrhs = irhs, maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit)
+                      optimal = STA(Constraints, info, xmat = imat, xdir = idir, xrhs = irhs, maximize = TRUE, mps = FALSE, lp = FALSE, verbosity = config@MIP$verbosity, time_limit = config@MIP$timeLimit, gap_limit = config@MIP$gapLimit, solver = config@MIP$solver)
                       output@shadowTestFeasible[position] = TRUE #safe assumption
                     }
                     
