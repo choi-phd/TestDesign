a02eac63a64221e67593f97a39d8e0cb0698d2df
choi-phd <44212658+choi-phd@users.noreply.github.com>
2019-06-12 10:09:15 -0500

    do a cleanup before publishing

--- R/Shadow_functions.R
+++ R/Shadow_functions.R
@@ -2072,25 +2072,18 @@ setMethod(f = "Shadow",
 #' @param trans A vector of integers between 0 and 255 where 0 is fully transparaent and 255 is fully visible.
 #'
 #' @export
-addTrans = function(color,trans)
+addTrans = function(color, trans)
 {
-  # This function adds transparancy to a color.
-  # Define transparancy with an integer between 0 and 255
-  # 0 being fully transparant and 255 being fully visable
-  # Works with either color and trans a vector of equal length,
-  # or one of the two of length 1.
-
-  if (length(color)!=length(trans)&!any(c(length(color),length(trans))==1)) stop("Vector lengths not correct")
-  if (length(color)==1 & length(trans)>1) color <- rep(color,length(trans))
-  if (length(trans)==1 & length(color)>1) trans <- rep(trans,length(color))
-
-  num2hex <- function(x)
+  if (length(color) != length(trans) & !any(c(length(color), length(trans)) == 1)) stop("Vector lengths not correct")
+  if (length(color) == 1 & length(trans) > 1) color = rep(color, length(trans))
+  if (length(trans) == 1 & length(color) > 1) trans = rep(trans, length(color))
+  num2hex = function(x)
   {
-    hex <- unlist(strsplit("0123456789ABCDEF",split=""))
-    return(paste(hex[(x-x%%16)/16+1],hex[x%%16+1],sep=""))
+    hex = unlist(strsplit("0123456789ABCDEF", split = ""))
+    return(paste(hex[(x-x%%16)/16+1], hex[x%%16+1], sep = ""))
   }
-  rgb <- rbind(col2rgb(color),trans)
-  res <- paste("#",apply(apply(rgb,2,num2hex),2,paste,collapse=""),sep="")
+  rgb = rbind(col2rgb(color), trans)
+  res = paste("#", apply(apply(rgb, 2, num2hex), 2, paste, collapse = ""), sep = "")
   return(res)
 }
 
