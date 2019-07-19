a3801d34d0939820004eed3ea5ad67e6362e0888
Sangdon Lim <sangdonlim@utexas.edu>
2019-07-19 17:25:30 -0500

    update

--- R/import.R
+++ R/import.R
@@ -10,7 +10,7 @@
 #' @importFrom logitnorm logit rlogitnorm
 #' @importFrom grDevices col2rgb dev.control dev.new dev.off pdf recordPlot
 #' @importFrom stats runif dnorm rlnorm rnorm sd
-#' @importFrom utils capture.output read.csv setTxtProgressBar txtProgressBar write.table
+#' @importFrom utils capture.output read.csv setTxtProgressBar txtProgressBar write.table packageVersion
 #' @importFrom graphics plot abline lines axis grid layout legend mtext par plot.new points rect text strheight
 #' @importFrom lattice xyplot
 #' @useDynLib oat
