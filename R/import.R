6950d2671c0c6b57d9ef9580605a88b6fde8d08c
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2019-07-22 16:35:42 -0500

    update

--- R/import.R
+++ R/import.R
@@ -32,13 +32,11 @@ NULL
 
     } else {
       status = red("x")
+      v = ""
     }
 
     packageStartupMessage(status, " ", s, paste0(rep(" ", 10 - nchar(s)), collapse = ""), white(v))
   }
-
-  packageStartupMessage(" ")
-  packageStartupMessage("  See ?dataset.science for examples.")
 }
 
 
