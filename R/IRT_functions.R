deee282fff0cff67194885f173820c5cd0f49dc8
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2019-07-25 16:31:00 -0500

    use roxygen templates

--- R/IRT_functions.R
+++ R/IRT_functions.R
@@ -19,7 +19,7 @@ setGeneric(name = "calcProb",
 #' @rdname calcProb-methods
 #' @aliases calcProb,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' prob.item.1 = calcProb(item.1, seq(-3, 3, 1))
 #' @template 1pl-ref
 setMethod(f = "calcProb",
@@ -35,7 +35,7 @@ setMethod(f = "calcProb",
 #' @rdname calcProb-methods
 #' @aliases calcProb,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' prob.item.2 = calcProb(item.2, seq(-3, 3, 1))
 #' @template 2pl-ref
 setMethod(f = "calcProb",
@@ -51,7 +51,7 @@ setMethod(f = "calcProb",
 #' @rdname calcProb-methods
 #' @aliases calcProb,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' prob.item.3 = calcProb(item.3, seq(-3, 3, 1))
 #' @template 3pl-ref
 setMethod(f = "calcProb",
@@ -67,7 +67,7 @@ setMethod(f = "calcProb",
 #' @rdname calcProb-methods
 #' @aliases calcProb,item.pc,numeric-method
 #' @examples
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' prob.item.4 = calcProb(item.4, seq(-3, 3, 1))
 #' @template pc-ref
 setMethod(f = "calcProb",
@@ -81,7 +81,7 @@ setMethod(f = "calcProb",
 #' @rdname calcProb-methods
 #' @aliases calcProb,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' prob.item.5 = calcProb(item.5, seq(-3, 3, 1))
 #' @template gpc-ref
 setMethod(f = "calcProb",
@@ -95,7 +95,7 @@ setMethod(f = "calcProb",
 #' @rdname calcProb-methods
 #' @aliases calcProb,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' prob.item.6 = calcProb(item.6, seq(-3, 3, 1))
 #' @template gr-ref
 setMethod(f = "calcProb",
@@ -143,7 +143,7 @@ setGeneric(name = "calcEscore",
 #' @rdname calcEscore-methods
 #' @aliases calcEscore,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' ICC.item.1 = calcEscore(item.1, seq(-3, 3, 1))
 #' @template 1pl-ref
 setMethod(f = "calcEscore",
@@ -156,7 +156,7 @@ setMethod(f = "calcEscore",
 #' @rdname calcEscore-methods
 #' @aliases calcEscore,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' ICC.item.2 = calcEscore(item.2, seq(-3, 3, 1))
 #' @template 2pl-ref
 setMethod(f = "calcEscore",
@@ -169,7 +169,7 @@ setMethod(f = "calcEscore",
 #' @rdname calcEscore-methods
 #' @aliases calcEscore,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' ICC.item.3 = calcEscore(item.3, seq(-3, 3, 1))
 #' @template 3pl-ref
 setMethod(f = "calcEscore",
@@ -182,7 +182,7 @@ setMethod(f = "calcEscore",
 #' @rdname calcEscore-methods
 #' @aliases calcEscore,item.pc,numeric-method
 #' @examples
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' ICC.item.4 = calcEscore(item.4, seq(-3, 3, 1))
 #' @template pc-ref
 setMethod(f = "calcEscore",
@@ -197,7 +197,7 @@ setMethod(f = "calcEscore",
 #' @rdname calcEscore-methods
 #' @aliases calcEscore,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' ICC.item.5 = calcEscore(item.5, seq(-3, 3, 1))
 #' @template gpc-ref
 setMethod(f = "calcEscore",
@@ -212,7 +212,7 @@ setMethod(f = "calcEscore",
 #' @rdname calcEscore-methods
 #' @aliases calcEscore,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' ICC.item.6 = calcEscore(item.6, seq(-3, 3, 1))
 #' @template gr-ref
 setMethod(f = "calcEscore",
@@ -261,7 +261,7 @@ setGeneric(name = "calcFisher",
 #' @rdname calcFisher-methods
 #' @aliases calcFisher,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' info.item.1 = calcFisher(item.1, seq(-3, 3, 1))
 #' @template 1pl-ref
 setMethod(f = "calcFisher",
@@ -275,7 +275,7 @@ setMethod(f = "calcFisher",
 #' @rdname calcFisher-methods
 #' @aliases calcFisher,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' info.item.2 = calcFisher(item.2, seq(-3, 3, 1))
 #' @template 2pl-ref
 setMethod(f = "calcFisher",
@@ -289,7 +289,7 @@ setMethod(f = "calcFisher",
 #' @rdname calcFisher-methods
 #' @aliases calcFisher,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' info.item.3 = calcFisher(item.3, seq(-3, 3, 1))
 #' @template 3pl-ref
 setMethod(f = "calcFisher",
@@ -302,7 +302,7 @@ setMethod(f = "calcFisher",
 
 #' @rdname calcFisher-methods
 #' @aliases calcFisher,item.pc,numeric-method
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' info.item.4 = calcFisher(item.4, seq(-3, 3, 1))
 #' @template pc-ref
 setMethod(f = "calcFisher",
@@ -316,7 +316,7 @@ setMethod(f = "calcFisher",
 #' @rdname calcFisher-methods
 #' @aliases calcFisher,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' info.item.5 = calcFisher(item.5, seq(-3, 3, 1))
 #' @template gpc-ref
 setMethod(f = "calcFisher",
@@ -330,7 +330,7 @@ setMethod(f = "calcFisher",
 #' @rdname calcFisher-methods
 #' @aliases calcFisher,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' info.item.6 = calcFisher(item.6, seq(-3, 3, 1))
 #' @template gr-ref
 setMethod(f = "calcFisher",
@@ -426,7 +426,7 @@ setGeneric(name = "calcLocation",
 #' @rdname calcLocation-methods
 #' @aliases calcLocation,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' theta.item.1 = calcLocation(item.1)
 #' @template 1pl-ref
 setMethod(f = "calcLocation",
@@ -439,7 +439,7 @@ setMethod(f = "calcLocation",
 #' @rdname calcLocation-methods
 #' @aliases calcLocation,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' theta.item.2 = calcLocation(item.2)
 #' @template 2pl-ref
 setMethod(f = "calcLocation",
@@ -452,7 +452,7 @@ setMethod(f = "calcLocation",
 #' @rdname calcLocation-methods
 #' @aliases calcLocation,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' theta.item.3 = calcLocation(item.3)
 #' @template 3pl-ref
 setMethod(f = "calcLocation",
@@ -466,7 +466,7 @@ setMethod(f = "calcLocation",
 #' @rdname calcLocation-methods
 #' @aliases calcLocation,item.pc,numeric-method
 #' @examples
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' theta.item.4 = calcLocation(item.4)
 #' @template pc-ref
 setMethod(f = "calcLocation",
@@ -480,7 +480,7 @@ setMethod(f = "calcLocation",
 #' @rdname calcLocation-methods
 #' @aliases calcLocation,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' theta.item.5 = calcLocation(item.5)
 #' @template gpc-ref
 setMethod(f = "calcLocation",
@@ -493,7 +493,7 @@ setMethod(f = "calcLocation",
 #' @rdname calcLocation-methods
 #' @aliases calcLocation,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' theta.item.6 = calcLocation(item.6)
 #' @template gr-ref
 setMethod(f = "calcLocation",
@@ -546,7 +546,7 @@ setGeneric(name = "calcDerivative",
 #' @rdname calcDerivative-methods
 #' @aliases calcDerivative,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' d.item.1 = calcDerivative(item.1, seq(-3, 3, 1))
 #' @template 1pl-ref
 setMethod(f = "calcDerivative",
@@ -561,7 +561,7 @@ setMethod(f = "calcDerivative",
 #' @rdname calcDerivative-methods
 #' @aliases calcDerivative,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' d.item.2 = calcDerivative(item.2, seq(-3, 3, 1))
 #' @template 2pl-ref
 setMethod(f = "calcDerivative",
@@ -576,7 +576,7 @@ setMethod(f = "calcDerivative",
 #' @rdname calcDerivative-methods
 #' @aliases calcDerivative,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' d.item.3 = calcDerivative(item.3, seq(-3, 3, 1))
 #' @template 3pl-ref
 setMethod(f = "calcDerivative",
@@ -591,7 +591,7 @@ setMethod(f = "calcDerivative",
 #' @rdname calcDerivative-methods
 #' @aliases calcDerivative,item.pc,numeric-method
 #' @examples
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' d.item.4 = calcDerivative(item.4, seq(-3, 3, 1))
 #' @template pc-ref
 setMethod(f = "calcDerivative",
@@ -610,7 +610,7 @@ setMethod(f = "calcDerivative",
 #' @rdname calcDerivative-methods
 #' @aliases calcDerivative,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' d.item.5 = calcDerivative(item.5, seq(-3, 3, 1))
 #' @template gpc-ref
 setMethod(f = "calcDerivative",
@@ -629,7 +629,7 @@ setMethod(f = "calcDerivative",
 #' @rdname calcDerivative-methods
 #' @aliases calcDerivative,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' d.item.6 = calcDerivative(item.6, seq(-3, 3, 1))
 #' @template gr-ref
 setMethod(f = "calcDerivative",
@@ -707,7 +707,7 @@ setGeneric(name = "calcDerivative2",
 #' @rdname calcDerivative2-methods
 #' @aliases calcDerivative2,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' dd.item.1 = calcDerivative2(item.1, seq(-3, 3, 1))
 #' @template 1pl-ref
 setMethod(f = "calcDerivative2",
@@ -722,7 +722,7 @@ setMethod(f = "calcDerivative2",
 #' @rdname calcDerivative2-methods
 #' @aliases calcDerivative2,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' dd.item.2 = calcDerivative2(item.2, seq(-3, 3, 1))
 #' @template 2pl-ref
 setMethod(f = "calcDerivative2",
@@ -737,7 +737,7 @@ setMethod(f = "calcDerivative2",
 #' @rdname calcDerivative2-methods
 #' @aliases calcDerivative2,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' dd.item.3 = calcDerivative2(item.3, seq(-3, 3, 1))
 #' @template 3pl-ref
 setMethod(f = "calcDerivative2",
@@ -752,7 +752,7 @@ setMethod(f = "calcDerivative2",
 #' @rdname calcDerivative2-methods
 #' @aliases calcDerivative2,item.pc,numeric-method
 #' @examples
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' dd.item.4 = calcDerivative2(item.4, seq(-3, 3, 1))
 #' @template pc-ref
 setMethod(f = "calcDerivative2",
@@ -771,7 +771,7 @@ setMethod(f = "calcDerivative2",
 #' @rdname calcDerivative2-methods
 #' @aliases calcDerivative2,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' dd.item.5 = calcDerivative2(item.5, seq(-3, 3, 1))
 #' @template gpc-ref
 setMethod(f = "calcDerivative2",
@@ -790,7 +790,7 @@ setMethod(f = "calcDerivative2",
 #' @rdname calcDerivative2-methods
 #' @aliases calcDerivative2,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' dd.item.6 = calcDerivative2(item.6, seq(-3, 3, 1))
 #' @template gr-ref
 setMethod(f = "calcDerivative2",
@@ -868,7 +868,7 @@ setGeneric(name = "calcJacobian",
 #' @rdname calcJacobian-methods
 #' @aliases calcJacobian,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' j.item.1 = calcJacobian(item.1, seq(-3, 3, 1), 0)
 #' @template 1pl-ref
 setMethod(f = "calcJacobian",
@@ -882,7 +882,7 @@ setMethod(f = "calcJacobian",
 #' @rdname calcJacobian-methods
 #' @aliases calcJacobian,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' j.item.2 = calcJacobian(item.2, seq(-3, 3, 1), 0)
 #' @template 2pl-ref
 setMethod(f = "calcJacobian",
@@ -896,7 +896,7 @@ setMethod(f = "calcJacobian",
 #' @rdname calcJacobian-methods
 #' @aliases calcJacobian,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' j.item.3 = calcJacobian(item.3, seq(-3, 3, 1), 0)
 #' @template 3pl-ref
 setMethod(f = "calcJacobian",
@@ -911,7 +911,7 @@ setMethod(f = "calcJacobian",
 #' @rdname calcJacobian-methods
 #' @aliases calcJacobian,item.pc,numeric-method
 #' @examples
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' j.item.4 = calcJacobian(item.4, seq(-3, 3, 1), 0)
 #' @template pc-ref
 setMethod(f = "calcJacobian",
@@ -925,7 +925,7 @@ setMethod(f = "calcJacobian",
 #' @rdname calcJacobian-methods
 #' @aliases calcJacobian,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' j.item.5 = calcJacobian(item.5, seq(-3, 3, 1), 0)
 #' @template gpc-ref
 setMethod(f = "calcJacobian",
@@ -939,7 +939,7 @@ setMethod(f = "calcJacobian",
 #' @rdname calcJacobian-methods
 #' @aliases calcJacobian,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' j.item.6 = calcJacobian(item.6, seq(-3, 3, 1), 0)
 #' @template gr-ref
 setMethod(f = "calcJacobian",
@@ -1015,7 +1015,7 @@ setGeneric(name = "calcHessian",
 #' @rdname calcHessian-methods
 #' @aliases calcHessian,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' h.item.1 = calcHessian(item.1, seq(-3, 3, 1), 0)
 #' @template 1pl-ref
 setMethod(f = "calcHessian",
@@ -1029,7 +1029,7 @@ setMethod(f = "calcHessian",
 #' @rdname calcHessian-methods
 #' @aliases calcHessian,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' h.item.2 = calcHessian(item.2, seq(-3, 3, 1), 0)
 #' @template 2pl-ref
 setMethod(f = "calcHessian",
@@ -1043,7 +1043,7 @@ setMethod(f = "calcHessian",
 #' @rdname calcHessian-methods
 #' @aliases calcHessian,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' h.item.3 = calcHessian(item.3, seq(-3, 3, 1), 0)
 #' @template 3pl-ref
 setMethod(f = "calcHessian",
@@ -1058,7 +1058,7 @@ setMethod(f = "calcHessian",
 #' @rdname calcHessian-methods
 #' @aliases calcHessian,item.pc,numeric-method
 #' @examples
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' h.item.4 = calcHessian(item.4, seq(-3, 3, 1), 0)
 #' @template pc-ref
 setMethod(f = "calcHessian",
@@ -1072,7 +1072,7 @@ setMethod(f = "calcHessian",
 #' @rdname calcHessian-methods
 #' @aliases calcHessian,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' h.item.5 = calcHessian(item.5, seq(-3, 3, 1), 0)
 #' @template gpc-ref
 setMethod(f = "calcHessian",
@@ -1086,7 +1086,7 @@ setMethod(f = "calcHessian",
 #' @rdname calcHessian-methods
 #' @aliases calcHessian,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' h.item.6 = calcHessian(item.6, seq(-3, 3, 1), 0)
 #' @template gr-ref
 setMethod(f = "calcHessian",
@@ -1165,7 +1165,7 @@ setGeneric(name = "simResp",
 #' @rdname simResp-methods
 #' @aliases simResp,item.1pl,numeric-method
 #' @examples
-#' item.1 = new("item.1pl", difficulty = 0.5)
+#' @template 1pl-new
 #' sim.item.1 = simResp(item.1, seq(-3, 3, 1))
 #' @template 1pl-ref
 setMethod(f = "simResp",
@@ -1182,7 +1182,7 @@ setMethod(f = "simResp",
 #' @rdname simResp-methods
 #' @aliases simResp,item.2pl,numeric-method
 #' @examples
-#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
+#' @template 2pl-new
 #' sim.item.2 = simResp(item.2, seq(-3, 3, 1))
 #' @template 2pl-ref
 setMethod(f = "simResp",
@@ -1199,7 +1199,7 @@ setMethod(f = "simResp",
 #' @rdname simResp-methods
 #' @aliases simResp,item.3pl,numeric-method
 #' @examples
-#' item.3 = new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+#' @template 3pl-new
 #' sim.item.3 = simResp(item.3, seq(-3, 3, 1))
 #' @template 3pl-ref
 setMethod(f = "simResp",
@@ -1216,7 +1216,7 @@ setMethod(f = "simResp",
 #' @rdname simResp-methods
 #' @aliases simResp,item.pc,numeric-method
 #' @examples
-#' item.4 = new("item.pc", threshold = c(-1, 0, 1), ncat = 4)
+#' @template pc-new
 #' sim.item.4 = simResp(item.4, seq(-3, 3, 1))
 #' @template pc-ref
 setMethod(f = "simResp",
@@ -1237,7 +1237,7 @@ setMethod(f = "simResp",
 #' @rdname simResp-methods
 #' @aliases simResp,item.gpc,numeric-method
 #' @examples
-#' item.5 = new("item.gpc", slope = 1.2, threshold = c(-0.8, -1.0, 0.5), ncat = 4)
+#' @template gpc-new
 #' sim.item.5 = simResp(item.5, seq(-3, 3, 1))
 #' @template gpc-ref
 setMethod(f = "simResp",
@@ -1258,7 +1258,7 @@ setMethod(f = "simResp",
 #' @rdname simResp-methods
 #' @aliases simResp,item.gr,numeric-method
 #' @examples
-#' item.6 = new("item.gr", slope = 0.9, category = c(-1, 0 , 1), ncat = 4)
+#' @template gr-new
 #' sim.item.6 = simResp(item.6, seq(-3, 3, 1))
 #' @template gr-ref
 setMethod(f = "simResp",
---
+++ man-roxygen/2pl-new.R
@@ -0,0 +1,1 @@
+#' item.2 = new("item.2pl", slope = 1.0, difficulty = 0.5)
