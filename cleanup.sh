f242c1e80e0bf9068c1e63fec0df25b02f22e347
Sangdon Lim <sangdonlim@utexas.edu>
2019-07-29 13:33:58 -0500

    update

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,8 @@ dos2unix -o man/*.*
 dos2unix -o R/*.*
 dos2unix -o data-raw/*.*
 dos2unix -o vignettes/*.*
+dos2unix -o tests/*.*
+dos2unix -o tests/testthat/*.*
+dos2unix -o DESCRIPTION
+dos2unix -o NAMESPACE
+dos2unix -o *.Rproj
