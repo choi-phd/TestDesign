b47e31d82e3eb8c24db7f8687032de01a0486566
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2019-08-05 14:34:22 -0500

    update figures

--- R/manuscript.R
+++ R/manuscript.R
@@ -1,12 +1,12 @@
 if (FALSE) {
   # IRT models
 
-  item_1 <- new("item.1pl", difficulty = 0.5)
-  item_2 <- new("item.2pl", slope = 1.0, difficulty = 0.5)
-  item_3 <- new("item.3pl", slope = 1.0, difficulty = 0.5, guessing = 0.2)
-  item_4 <- new("item.pc", threshold = c(-0.5, 0.5), ncat = 3)
-  item_5 <- new("item.gpc", slope = 1.0, threshold = c(-0.5, 0.0, 0.5), ncat = 4)
-  item_6 <- new("item.gr", slope = 1.0, category = c(-1, 0, 1), ncat = 4)
+  item_1 <- new("item_1PL", difficulty = 0.5)
+  item_2 <- new("item_2PL", slope = 1.0, difficulty = 0.5)
+  item_3 <- new("item_3PL", slope = 1.0, difficulty = 0.5, guessing = 0.2)
+  item_4 <- new("item_PC", threshold = c(-0.5, 0.5), ncat = 3)
+  item_5 <- new("item_GPC", slope = 1.0, threshold = c(-0.5, 0.0, 0.5), ncat = 4)
+  item_6 <- new("item_GR", slope = 1.0, category = c(-1, 0, 1), ncat = 4)
 
   calcProb(item_1, 0)
   calcFisher(item_2, 0)
@@ -18,7 +18,7 @@ if (FALSE) {
 
   # Manuscript figures
 
-  config_science <- createStaticTestConfig(itemSelection = list(method = "MAXINFO", targetLocation = c(-1, 0, 1)))
+  config_science <- createStaticTestConfig(item_selection = list(method = "MAXINFO", target_location = c(-1, 0, 1)))
   solution <- ATA(config_science, constraints_science, plot = T)
   solution$selected
 
@@ -28,7 +28,7 @@ if (FALSE) {
   dev.off()
 
 
-  config_science <- createStaticTestConfig(itemSelection = list(method = "TCC", targetValue = c(15, 20, 25), targetLocation = c(-1, 0, 1)))
+  config_science <- createStaticTestConfig(item_selection = list(method = "TCC", target_value = c(15, 20, 25), target_location = c(-1, 0, 1)))
   solution <- ATA(config_science, constraints_science, plot = T)
   solution$selected
 
@@ -41,24 +41,15 @@ if (FALSE) {
 
   set.seed(1)
 
-  true_theta <- runif(7000, min = -3.5, max = 3.5)
-  resp_science <- makeTest(itempool_science, infoType = "FISHER", true_theta = true_theta)@Data
+  true_theta <- runif(70, min = -3.5, max = 3.5)
+  resp_science <- makeTest(itempool_science, info_type = "FISHER", true_theta = true_theta)@data
+  config_science <- createShadowTestConfig(MIP = list(solver = "LPSOLVE"), exposure_control = list(method = "ELIGIBILITY"))
+  constraints_science <- updateConstraints(constraints_science, off = c(14:20, 32:36))
 
-  config_science <- config.Shadow(MIP = list(solver = "LPSOLVE"), exposureControl = list(method = "ELIGIBILITY"))
-
-  constraints_science2 <- updateConstraints(constraints_science, off = c(14:20, 32:36))
-
-  solution <- Shadow(itempool_science, config_science, true_theta, constraints_science2, Data = resp_science)
-
-  tt <- rep(NA, 7000)
-
-  for (i in 1:7000) {
-    tt[i] <- sum(solution$output[[i]]@solveTime)
-  }
-
+  solution <- Shadow(itempool_science, config_science, true_theta, constraints_science, data = resp_science)
 
   setEPS()
-  postscript("R/science.exposure.eps", width = 8, height = 4.25)
+  postscript("R/science_exposure.eps", width = 8, height = 4.25)
   par(mfrow = c(2, 4))
   plotExposure(solution)
   dev.off()
@@ -68,25 +59,23 @@ if (FALSE) {
 
   set.seed(1)
 
-  true_theta <- runif(1, min = -3.5, max = 3.5)
-  resp_science <- makeTest(itempool_science, true_theta = true_theta)@Data
-
-  config_science <- config.Shadow()
-
+  true_theta     <- runif(1, min = -3.5, max = 3.5)
+  resp_science   <- makeTest(itempool_science, true_theta = true_theta)@data
+  config_science <- createShadowTestConfig()
   solution <- Shadow(itempool_science, config_science,
     true_theta, constraints_science,
-    Data = resp_science
+    data = resp_science
   )
 
   setEPS()
-  postscript("R/science.shadowplot.eps", width = 8, height = 9.7)
-  p <- plotShadow(solution, constraints_science, examineeID = 1)
+  postscript("R/science_shadowplot.eps", width = 8, height = 9.7)
+  p <- plotShadow(solution, constraints_science, examinee_id = 1)
   p
   dev.off()
 
   setEPS()
-  postscript("R/science.auditplot.eps", width = 8, height = 4.2)
-  p <- plotCAT(solution, examineeID = 1, minTheta = -4, maxTheta = 1.5)
+  postscript("R/science_auditplot.eps", width = 8, height = 4.2)
+  p <- plotCAT(solution, examinee_id = 1, min_theta = -4, max_theta = 1.5)
   print(p)
   dev.off()
 
@@ -94,32 +83,32 @@ if (FALSE) {
 
   # Manuscript example: adaptive test assembly (reading)
 
-  config.reading <- createStaticTestConfig(itemSelection = list(method = "MAXINFO", targetLocation = c(-1, 0, 1)))
+  config_reading <- createStaticTestConfig(item_selection = list(method = "MAXINFO", target_location = c(-1, 0, 1)))
 
-  solution <- ATA(config.reading, constraints.reading, plot = T)
+  solution <- ATA(config_reading, constraints_reading, plot = T)
   solution$selected
   solution$plot
 
   set.seed(1)
 
   true_theta <- runif(1, min = -3.5, max = 3.5)
-  resp.reading <- makeTest(itempool.reading, true_theta = true_theta)@Data
+  resp_reading <- makeTest(itempool_reading, true_theta = true_theta)@data
 
-  config.reading <- config.Shadow(refreshPolicy = list(
+  config_reading <- createShadowTestConfig(refresh_policy = list(
     method = "STIMULUS"
   ))
 
-  solution <- Shadow(itempool.reading, config.reading, NULL, constraints.reading, Data = resp.reading)
+  solution <- Shadow(itempool_reading, config_reading, NULL, constraints_reading, data = resp_reading)
 
   setEPS()
-  postscript("R/reading.auditplot.eps", width = 8, height = 4.2)
-  p <- plotCAT(solution, 1, maxTheta = 3)
+  postscript("R/reading_auditplot.eps", width = 8, height = 4.2)
+  p <- plotCAT(solution, 1, max_theta = 3)
   p
   dev.off()
 
   setEPS()
-  postscript("R/reading.shadowplot.eps", width = 8, height = 9)
-  p <- plotShadow(solution, constraints.reading, 1)
+  postscript("R/reading_shadowplot.eps", width = 8, height = 9)
+  p <- plotShadow(solution, constraints_reading, 1)
   p
   dev.off()
 
@@ -127,18 +116,18 @@ if (FALSE) {
   # Fatigue
 
   setEPS()
-  postscript("R/fatigue.infoplot.eps", width = 8, height = 3.25)
-  p <- maxinfoplot(itempool.fatigue, constraints.fatigue)
+  postscript("R/fatigue_infoplot.eps", width = 8, height = 3.25)
+  p <- plotMaxInfo(itempool_fatigue, constraints_fatigue)
   print(p)
   dev.off()
 
-  config.fatigue <- createStaticTestConfig(itemSelection = list(method = "TIF", targetValue = c(40, 40, 40), targetLocation = c(0, 1, 2)))
+  config_fatigue <- createStaticTestConfig(item_selection = list(method = "TIF", target_value = c(40, 40, 40), target_location = c(0, 1, 2)))
 
-  solution <- ATA(config.fatigue, constraints.fatigue, plot = T)
+  solution <- ATA(config_fatigue, constraints_fatigue, plot = T)
   solution$selected
 
   setEPS()
-  postscript("R/fatigue.tif.eps", width = 8, height = 3.25)
+  postscript("R/fatigue_tif.eps", width = 8, height = 3.25)
   solution$plot
   dev.off()
 
@@ -148,14 +137,14 @@ if (FALSE) {
   set.seed(1)
 
   true_theta <- runif(1, min = -3.5, max = 3.5)
-  respData <- makeTest(itempool.fatigue, true_theta = true_theta)@Data
+  resp_fatigue <- makeTest(itempool_fatigue, true_theta = true_theta)@data
 
-  config.fatigue <- config.Shadow(refreshPolicy = list(
+  config_fatigue <- createShadowTestConfig(refresh_policy = list(
     method = "POSITION",
     position = c(1, 5, 9)
   ))
 
-  solution <- Shadow(itempool.fatigue, config.fatigue, true_theta, constraints.fatigue, Data = respData)
+  solution <- Shadow(itempool_fatigue, config_fatigue, true_theta, constraints_fatigue, data = resp_fatigue)
 
   setEPS()
   postscript("R/fatigue.auditplot.eps", width = 8, height = 8)
@@ -165,11 +154,8 @@ if (FALSE) {
 
   setEPS()
   postscript("R/fatigue.shadowplot.eps", width = 8, height = 6)
-  p <- plotShadow(solution, constraints.fatigue, 1)
+  p <- plotShadow(solution, constraints_fatigue, 1)
   p
   dev.off()
-
-  # Paper Note: ATA with specifying a target amount of information
-  # to prevent item exposure and information overuse
 
 }
