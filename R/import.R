8a31888787c2eaee11a5925722ac11b275011068
Sangdon Lim <sangdonlim@utexas.edu>
2019-10-30 13:16:47 -0500

    update

--- R/import.R
+++ R/import.R
@@ -10,6 +10,15 @@
 # Use snake_case for global objects (example datasets).
 # Use snake_case for S3 list slot names and S4 class slot names.
 
+# Error message conventions
+# Use sentence case. (capitalize the first letter and end with a period.)
+# Use single quotes for all non-slot references.
+# Retain original capitalizations of references (e.g. (x) "'Theta'", (o) "'theta'").
+# # Prioritize this when beginning a sentence with reference.
+# Use appropriate slot operators (@, $) for slot references. Do not encapsulate with quotes.
+# Do not disambiguate references. (e.g. (x) "Argument 'x' must be blah.", (o) "'x' must be blah.")
+# Always give full names for slot references. (e.g. "@foo$bar")
+
 #' @import Matrix
 #' @import lpSolve
 #' @import Rcpp methods
@@ -19,9 +28,9 @@
 #' @importFrom methods new show validObject
 #' @importFrom logitnorm logit rlogitnorm
 #' @importFrom grDevices col2rgb dev.control dev.new dev.off pdf recordPlot
-#' @importFrom stats runif dnorm rlnorm rnorm sd
+#' @importFrom stats runif dnorm rlnorm rnorm sd na.omit
 #' @importFrom utils capture.output read.csv setTxtProgressBar txtProgressBar write.table packageVersion
-#' @importFrom graphics plot abline lines axis grid layout legend mtext par plot.new points rect text strheight
+#' @importFrom graphics plot abline lines axis grid layout legend mtext par plot.new points rect text strheight box
 #' @useDynLib TestDesign
 NULL
 
@@ -30,7 +39,7 @@ NULL
   packageStartupMessage(white(bold("  Solver packages:")))
   packageStartupMessage(" ")
 
-  solver_names <- c("lpSolve", "Rsymphony", "gurobi", "Rglpk")
+  solver_names <- c("lpsymphony", "Rsymphony", "gurobi", "lpSolve", "Rglpk")
 
   for (s in solver_names) {
     x <- find.package(s, quiet = TRUE)
@@ -41,8 +50,13 @@ NULL
       status <- red("x")
       v <- ""
     }
-    packageStartupMessage(status, " ", s, paste0(rep(" ", 10 - nchar(s)), collapse = ""), white(v))
+    packageStartupMessage(status, " ", s, paste0(rep(" ", 11 - nchar(s)), collapse = ""), white(v))
   }
 }
 
+setClassUnion("dataframe_or_null", c("data.frame", "NULL"))
+setClassUnion("character_or_null", c("character",  "NULL"))
+setClassUnion("numeric_or_null",   c("numeric",    "NULL"))
+setClassUnion("matrix_or_null",    c("matrix",     "NULL"))
+setClassUnion("list_or_null",      c("list",     "NULL"))
 
--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # TestDesign
 Optimal **Test Design** Approach to Fixed and Adaptive Test Construction
 
+
  Founded on the optimal test design framework, the `TestDesign` package allows for constructing fixed test forms and simulating adaptive tests to the same test specifications with complex sets of constraints. This approach can render a variety of testing formats with different levels of adaptativeness and relative efficiency. This package provides several item banks from real-world testing scenarios.
  
 <!-- badges: start -->
-[![Travis build status](https://travis-ci.org/choi-phd/TestDesign.svg?branch=master)](https://travis-ci.org/choi-phd/TestDesign)
+[![Travis build status](https://travis-ci.com/choi-phd/TestDesign-devel.svg?branch=master)](https://travis-ci.com/choi-phd/TestDesign-devel)
+<!-- [![Travis build status](https://travis-ci.org/choi-phd/TestDesign.svg?branch=master)](https://travis-ci.org/choi-phd/TestDesign)
+-->
 [![](https://cranlogs.r-pkg.org/badges/grand-total/TestDesign?color=lightgrey)](https://cran.r-project.org/package=TestDesign)
 <!-- badges: end -->
