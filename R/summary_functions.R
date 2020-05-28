53177ebafb6374fb48946c47a9120168b7f24b75
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-05-27 23:31:59 -0500

    add summary method for output_Static

--- R/summary_functions.R
+++ R/summary_functions.R
@@ -25,3 +25,24 @@ setMethod("summary", "item_attrib", function(object) {
   }
   return(out)
 })
+
+#' @aliases summary,output_Static-method
+#' @docType methods
+#' @rdname summary-methods
+setMethod("summary", "output_Static", function(object) {
+  out <- new("summary_output_Static")
+  out@n_targets        <- length(object@config@item_selection$target_location)
+  out@obj_type         <- object@config@item_selection$method
+  out@target_location  <- object@config@item_selection$target_location
+  out@selected_items   <- object@selected[['INDEX']]
+  out@set_based        <- object@constraints@set_based
+  out@n_selected_sets  <- NULL
+  if (out@set_based) {
+    out@n_selected_sets <- length(unique(object@selected[['STID']]))
+  }
+  subpool   <- subsetItemPool(object@pool, select = out@selected_items)
+  info      <- calcFisher(subpool, out@target_location)
+  out@info  <- rowSums(info)
+  out@score <- calcEscore(subpool, out@target_location)
+  return(out)
+})
