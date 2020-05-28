07d07d1abf909502428e75823ae6f2155bc696d7
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-05-27 21:25:26 -0500

    add output_Static

--- tests/testthat/test-static.R
+++ tests/testthat/test-static.R
@@ -9,35 +9,35 @@ test_that("Static", {
         MIP = list(solver = solver_names[i])
       )
       solution <- Static(config_science, constraints_science)
-      expect_equal(dim(solution$selected)[1], 30)
+      expect_equal(dim(solution@selected)[1], 30)
 
       config_science <- createStaticTestConfig(
         item_selection = list(method = "TIF", target_location = c(-1, 0, 1), target_value = c(20, 20, 20)),
         MIP = list(solver = solver_names[i])
       )
       solution <- Static(config_science, constraints_science)
-      expect_equal(dim(solution$selected)[1], 30)
+      expect_equal(dim(solution@selected)[1], 30)
 
       config_science <- createStaticTestConfig(
         item_selection = list(method = "TCC", target_location = c(-1, 0, 1), target_value = c(10, 20, 30)),
         MIP = list(solver = solver_names[i])
       )
       solution <- Static(config_science, constraints_science)
-      expect_equal(dim(solution$selected)[1], 30)
+      expect_equal(dim(solution@selected)[1], 30)
 
       config_reading <- createStaticTestConfig(
         item_selection = list(method = "MAXINFO", target_location = c(-2, 2)),
         MIP = list(solver = solver_names[i])
       )
       solution <- Static(config_reading, constraints_reading)
-      expect_equal(dim(solution$selected)[1], 30)
+      expect_equal(dim(solution@selected)[1], 30)
 
       config_reading <- createStaticTestConfig(
         item_selection = list(method = "TIF", target_location = c(1, 2), target_value = c(10, 30)),
         MIP = list(solver = solver_names[i])
       )
       solution <- Static(config_reading, constraints_reading)
-      expect_equal(dim(solution$selected)[1], 30)
+      expect_equal(dim(solution@selected)[1], 30)
     }
   }
 })
