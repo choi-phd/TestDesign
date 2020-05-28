063e9890416217f3f44c0571fa46e9a0d7e335ea
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-05-27 23:13:49 -0500

    add print method for item classes

--- R/print_functions.R
+++ R/print_functions.R
@@ -1,3 +1,65 @@
 #' @include summary_class.R
 # Print functions should return the argument 'x' invisibly.
 NULL
+
+#' @aliases print,item_1PL-method
+#' @docType methods
+#' @rdname print-methods
+setMethod("print", "item_1PL", function(x) {
+  cat("One-parameter logistic or Rasch model (item_1PL)\n")
+  cat("  Difficulty     :", x@difficulty, "\n")
+  return(invisible(x))
+})
+
+#' @aliases print,item_2PL-method
+#' @docType methods
+#' @rdname print-methods
+setMethod("print", "item_2PL", function(x) {
+  cat("Two-parameter logistic model (item_2PL) \n")
+  cat("  Slope          :", x@slope, "\n")
+  cat("  Difficulty     :", x@difficulty, "\n")
+  return(invisible(x))
+})
+
+#' @aliases print,item_3PL-method
+#' @docType methods
+#' @rdname print-methods
+setMethod("print", "item_3PL", function(x) {
+  cat("Three-parameter logistic model (item_3PL)\n")
+  cat("  Slope          :", x@slope, "\n")
+  cat("  Difficulty     :", x@difficulty, "\n")
+  cat("  Guessing       :", x@guessing, "\n")
+  return(invisible(x))
+})
+
+#' @aliases print,item_PC-method
+#' @docType methods
+#' @rdname print-methods
+setMethod("print", "item_PC", function(x) {
+  cat("Partial credit model (item_PC)\n")
+  cat("  Threshold     :", x@threshold, "\n")
+  cat("  N categories  :", x@ncat, "\n")
+  return(invisible(x))
+})
+
+#' @aliases print,item_GPC-method
+#' @docType methods
+#' @rdname print-methods
+setMethod("print", "item_GPC", function(x) {
+  cat("Generalized partial credit model (item_GPC)\n")
+  cat("  Slope         :", x@slope, "\n")
+  cat("  Threshold     :", x@threshold, "\n")
+  cat("  N categories  :", x@ncat, "\n")
+  return(invisible(x))
+})
+
+#' @aliases print,item_GR-method
+#' @docType methods
+#' @rdname print-methods
+setMethod("print", "item_GR", function(x) {
+  cat("Graded response model (item_GR)\n")
+  cat("  Slope         :", x@slope, "\n")
+  cat("  Category b    :", x@category, "\n")
+  cat("  N categories  :", x@ncat, "\n")
+  return(invisible(x))
+})
--- R/show_functions.R
+++ R/show_functions.R
@@ -2,3 +2,51 @@
 # Show functions should return NULL invisibly.
 # Always call print() internally.
 NULL
+
+#' @aliases show,item_1PL-method
+#' @docType methods
+#' @rdname show-methods
+setMethod("show", "item_1PL", function(object) {
+  print(object)
+  return(invisible(NULL))
+})
+
+#' @aliases show,item_2PL-method
+#' @docType methods
+#' @rdname show-methods
+setMethod("show", "item_2PL", function(object) {
+  print(object)
+  return(invisible(NULL))
+})
+
+#' @aliases show,item_3PL-method
+#' @docType methods
+#' @rdname show-methods
+setMethod("show", "item_3PL", function(object) {
+  print(object)
+  return(invisible(NULL))
+})
+
+#' @aliases show,item_PC-method
+#' @docType methods
+#' @rdname show-methods
+setMethod("show", "item_PC", function(object) {
+  print(object)
+  return(invisible(NULL))
+})
+
+#' @aliases show,item_GPC-method
+#' @docType methods
+#' @rdname show-methods
+setMethod("show", "item_GPC", function(object) {
+  print(object)
+  return(invisible(NULL))
+})
+
+#' @aliases show,item_GR-method
+#' @docType methods
+#' @rdname show-methods
+setMethod("show", "item_GR", function(object) {
+  print(object)
+  return(invisible(NULL))
+})
