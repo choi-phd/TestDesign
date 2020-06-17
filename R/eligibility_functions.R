27895d31bd500eaf28e96725d28b77decaf7a289
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-06-16 22:20:13 -0500

    add applyIncrement

--- R/eligibility_functions.R
+++ R/eligibility_functions.R
@@ -136,7 +136,7 @@ getEligibleFlagInSegment <- function(ineligible_flag, segment, constants) {
 }
 
 #' @noRd
-applyIncrement <- function(o, segments_to_apply, segment_prob, theta_is_feasible, x, exposure_constants, constants) {
+applyIncrement <- function(o, segments_to_apply, segment_prob, theta_is_feasible, eligible_flag, x, exposure_constants, constants) {
 
   fading_factor <- exposure_constants$fading_factor
 
@@ -158,18 +158,26 @@ applyIncrement <- function(o, segments_to_apply, segment_prob, theta_is_feasible
     o$a_ijk_nofade[segments_to_apply, administered_i] <-
     o$a_ijk_nofade[segments_to_apply, administered_i] + segment_prob
   }
+  o$r_ijk <- o$r_ijk + eligible_flag$i * segments_to_apply * segment_prob
+  if (exposure_constants$fading_factor != 1) {
+    o$r_ijk_nofade <- o$r_ijk_nofade + eligible_flag$i * segments_to_apply * segment_prob
+  }
 
   if (!constants$set_based) {
     return(o)
   }
 
-  administered_s <- x@administered_stimulus_index
+  administered_s <- na.omit(x@administered_stimulus_index)
 
   o$a_sjk[segments_to_apply, administered_s] <-
   o$a_sjk[segments_to_apply, administered_s] + segment_prob
   if (fading_factor != 1) {
     o$a_sjk_nofade[segments_to_apply, administered_s] <-
     o$a_sjk_nofade[segments_to_apply, administered_s] + segment_prob
+  }
+  o$r_sjk <- o$r_sjk + eligible_flag$s * segments_to_apply * segment_prob
+  if (exposure_constants$fading_factor != 1) {
+    o$r_sjk_nofade <- o$r_sjk_nofade + eligible_flag$s * segments_to_apply * segment_prob
   }
 
   return(o)
