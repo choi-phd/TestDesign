6817bc9d566ab9d433413809cc3858299bb0a010
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-07-05 15:37:20 -0500

    add sanitizeModel

--- R/other_functions.R
+++ R/other_functions.R
@@ -1,2 +1,14 @@
 #' @include shadow_functions.R
 NULL
+
+#' @noRd
+sanitizeModel <- function(model) {
+  model[which(model == "item_1PL")] <- 1
+  model[which(model == "item_2PL")] <- 2
+  model[which(model == "item_3PL")] <- 3
+  model[which(model == "item_PC")]  <- 4
+  model[which(model == "item_GPC")] <- 5
+  model[which(model == "item_GR")]  <- 6
+  model <- as.numeric(model)
+  return(model)
+}
