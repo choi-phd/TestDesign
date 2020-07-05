1dd1a11b0a9ddc1c6e5125d1cf0f7150fb6aab53
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-07-05 15:37:36 -0500

    add initializeTheta

--- R/theta_functions.R
+++ R/theta_functions.R
@@ -1,2 +1,13 @@
 #' @include shadow_functions.R
 NULL
+
+#' @noRd
+initializeTheta <- function(config, constants, posterior_record) {
+  nj <- constants$nj
+  if (!is.null(config@item_selection$initial_theta)) {
+    theta <- rep(config@item_selection$initial_theta, nj)
+  } else {
+    theta <- as.vector(posterior_record$posterior %*% matrix(constants$theta_q, ncol = 1))
+  }
+  return(theta)
+}
