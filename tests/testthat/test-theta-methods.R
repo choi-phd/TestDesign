15c7f17c01526212af560da35f44706c28d7c8b4
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-07-05 15:37:14 -0500

    add unit tests

---
+++ tests/testthat/test-theta-methods.R
@@ -0,0 +1,42 @@
+set.seed(1)
+true_theta <- runif(10, -3, 3)
+resp_bayes <- makeTest(itempool_bayes, info_type = "FISHER", true_theta = true_theta)@data
+
+methods <- c("EAP", "MLE", "EB", "FB")
+
+for (m in methods) {
+
+  test_name <- sprintf("interim %s works", m)
+  test_that(test_name, {
+
+    cfg <- createShadowTestConfig(
+      MIP = list(solver = "LPSOLVE"),
+      interim_theta = list(method = m)
+    )
+    set.seed(1)
+    solution <- Shadow(cfg, constraints_bayes, true_theta, data = resp_bayes)
+    last_interim_theta <- unlist(lapply(solution@output, function(x) x@interim_theta_est[30]))
+    fit <- lm(last_interim_theta ~ true_theta)
+    expect_gt(coef(fit)[2], 0.7)
+
+  })
+
+}
+
+for (m in methods) {
+
+  test_name <- sprintf("final %s works", m)
+  test_that(test_name, {
+
+    cfg <- createShadowTestConfig(
+      MIP = list(solver = "LPSOLVE"),
+      final_theta = list(method = m)
+    )
+    set.seed(1)
+    solution <- Shadow(cfg, constraints_bayes, true_theta, data = resp_bayes)
+    fit <- cor(solution@final_theta_est, true_theta)
+    expect_gt(fit, 0.7)
+
+  })
+
+}
