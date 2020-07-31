5c856a59e6aa60d515a8f745dc50793f0e2984d6
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-07-30 23:53:36 -0500

    add unit tests for when not using exposure control

--- tests/testthat/test_exposure_methods.R
+++ tests/testthat/test_exposure_methods.R
@@ -9,6 +9,18 @@ test_that("exposure control works", {
 
   cfg <- createShadowTestConfig(
     MIP = list(solver = "LPSOLVE"),
+    exposure_control = list(method = "NONE")
+  )
+  set.seed(1)
+  solution <- Shadow(cfg, constraints_bayes, true_theta, data = resp_bayes)
+  exposure_rate <- solution@exposure_rate[, 2]
+
+  expect_gt(
+    max(exposure_rate), 0.35
+  )
+
+  cfg <- createShadowTestConfig(
+    MIP = list(solver = "LPSOLVE"),
     exposure_control = list(method = "ELIGIBILITY")
   )
   set.seed(1)
