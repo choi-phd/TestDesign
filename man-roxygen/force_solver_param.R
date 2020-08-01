f2e72b7d51724efa0e3518cdad8321e129590d02
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-08-01 13:53:17 -0500

    add validateSolver

---
+++ man-roxygen/force_solver_param.R
@@ -0,0 +1,1 @@
+#' @param force_solver if \code{TRUE}, do not check whether the solver is one of recommended solvers for doing set-based assembly. Has no effect on discrete assembly. (default = \code{FALSE})
