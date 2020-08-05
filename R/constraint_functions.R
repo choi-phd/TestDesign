81c0d1be443039acf9e798ae03a75bbdc8ccc36f
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2020-08-05 13:46:35 -0500

    recreate ONOFF if needed

--- R/constraint_functions.R
+++ R/constraint_functions.R
@@ -6,6 +6,9 @@ normalizeConstraintData <- function(x) {
   x$TYPE      <- toupper(x$TYPE)
   x$WHAT      <- toupper(x$WHAT)
   x$COUNT     <- NA
+  if (is.null(x$ONOFF)) {
+    x$ONOFF <- NA
+  }
   x$ONOFF     <- toupper(x$ONOFF)
   x$ONOFF[is.na(x$ONOFF)] <- ""
   x$CONDITION <- trimws(x$CONDITION)
