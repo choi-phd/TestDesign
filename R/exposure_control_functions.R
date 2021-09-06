da768d32ecf38141f2b0c3a54c3a87384e0dfe42
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2021-09-06 15:32:25 -0500

    merge getExposureConstants() into getConstants()

--- R/exposure_control_functions.R
+++ R/exposure_control_functions.R
@@ -2,33 +2,6 @@
 NULL
 
 #' @noRd
-getExposureConstants <- function(exposure_control) {
-
-  o <- list()
-
-  if (toupper(exposure_control$method) %in% c("ELIGIBILITY", "BIGM", "BIGM-BAYESIAN")) {
-    o$use_eligibility_control <- TRUE
-  } else {
-    o$use_eligibility_control <- FALSE
-  }
-
-  o$max_exposure_rate   <- exposure_control$max_exposure_rate
-  o$fading_factor       <- exposure_control$fading_factor
-  o$acceleration_factor <- exposure_control$acceleration_factor
-  o$n_segment           <- exposure_control$n_segment
-  o$segment_cut         <- exposure_control$segment_cut
-  o$cut_lower           <- o$segment_cut[(1:o$n_segment)]
-  o$cut_upper           <- o$segment_cut[(1:o$n_segment) + 1]
-
-  if (!length(o$max_exposure_rate) %in% c(1, o$n_segment)) {
-    stop("length(max_exposure_rate) must be 1 or n_segment")
-  }
-
-  return(o)
-
-}
-
-#' @noRd
 initializeSegmentRecord <- function(exposure_constants, constants) {
 
   o <- list()
