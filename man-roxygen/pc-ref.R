16d31c815543e032161b0443189bf99985cfab4d
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2021-09-10 15:40:51 -0500

    fix hyphens

--- man-roxygen/pc-ref.R
+++ man-roxygen/pc-ref.R
@@ -1,6 +1,6 @@
 #' @references
 #' Masters, G. N. (1982). A Rasch model for partial credit scoring.
-#' \emph{Psychometrika, 47}(2), 149–174.
+#' \emph{Psychometrika, 47}(2), 149-174.
 #'
 #' Andrich, D. (1978). A rating formulation for ordered response categories.
-#' \emph{Psychometrika, 43}(4), 561–573.
+#' \emph{Psychometrika, 43}(4), 561-573.
