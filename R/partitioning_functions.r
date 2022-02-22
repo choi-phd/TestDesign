395c8e3f5eda999fdae89775794f3efe63598941
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2022-02-22 14:44:33 -0600

    add dbind()

--- R/partitioning_functions.r
+++ R/partitioning_functions.r
@@ -1,2 +1,30 @@
 #' @include shadow_functions.R
 NULL
+
+#' @noRd
+dbind <- function(...) {
+
+  x <- list(...)
+
+  n_rows <- lapply(x, function(xx) dim(xx)[1])
+  n_cols <- lapply(x, function(xx) dim(xx)[2])
+  n_rows_total <- sum(unlist(n_rows))
+  n_cols_total <- sum(unlist(n_cols))
+
+  o <- matrix(0, n_rows_total, n_cols_total)
+
+  leftpad_row <- 0
+  leftpad_col <- 0
+
+  for (i in 1:length(x)) {
+    o[
+      leftpad_row + (1:n_rows[[i]]),
+      leftpad_col + (1:n_cols[[i]])
+    ] <- x[[i]]
+    leftpad_row <- leftpad_row + n_rows[[i]]
+    leftpad_col <- leftpad_col + n_cols[[i]]
+  }
+
+  return(o)
+
+}
