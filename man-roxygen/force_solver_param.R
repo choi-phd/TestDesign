d6959e599c853cbbe6f77bf7f7d2793c07e7ab35
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2022-02-22 15:56:20 -0600

    extend validateSolver()

--- man-roxygen/force_solver_param.R
+++ man-roxygen/force_solver_param.R
@@ -1,1 +1,1 @@
-#' @param force_solver if \code{TRUE}, do not check whether the solver is one of recommended solvers for doing set-based assembly. Has no effect on discrete assembly. (default = \code{FALSE})
+#' @param force_solver if \code{TRUE}, do not check whether the solver is one of recommended solvers for complex problems (set-based assembly, partitioning). (default = \code{FALSE})
