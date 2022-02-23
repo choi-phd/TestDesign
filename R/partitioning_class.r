33acd3215093b573b4af7ffb7ebc2bb8dc364f59
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2022-02-23 15:40:30 -0600

    add output_Split class

--- R/partitioning_class.r
+++ R/partitioning_class.r
@@ -1,2 +1,42 @@
 #' @include shadow_functions.R
 NULL
+
+#' Class 'output_Split': partitioning solution
+#'
+#' \code{\linkS4class{output_Split}} is an S4 class to represent the partitioning solution of an item pool.
+#'
+#' @slot output a list containing item/set indices of each partition.
+#' @slot feasible for partitioning into pools, \code{TRUE} indicates the complete assignment problem was feasible.
+#' @slot solve_time elapsed time in running the solver.
+#' @slot set_based whether the item pool was set-based.
+#' @slot config the \code{\linkS4class{config_Static}} used in the assembly.
+#' @slot constraints the \code{\linkS4class{constraints}} used in the assembly.
+#' @slot partition_size_range the partition size range for splitting into pools.
+#' @slot partition_type the partition type. Can be a \code{test} or a \code{pool}.
+#'
+#' @export
+setClass("output_Split",
+  slots = c(
+    output               = "list",
+    feasible             = "logical",
+    solve_time           = "numeric",
+    set_based            = "logical",
+    config               = "config_Static",
+    constraints          = "constraints",
+    partition_size_range = "numeric",
+    partition_type       = "character"
+  ),
+  prototype = list(
+    output               = list(0),
+    feasible             = logical(0),
+    solve_time           = numeric(0),
+    set_based            = logical(0),
+    config               = new("config_Static"),
+    constraints          = new("constraints"),
+    partition_size_range = numeric(0),
+    partition_type       = character(0)
+  ),
+  validity = function(object) {
+    return(TRUE)
+  }
+)
