05daa5bf615c28bee569ab9f69c9b594ed1636fa
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2022-02-23 18:03:35 -0600

    update test for Split()

--- tests/testthat/test_partitioning_methods.r
+++ tests/testthat/test_partitioning_methods.r
@@ -3,6 +3,8 @@ test_that("partitioning methods work", {
   skip_on_cran()
   skip_on_ci()
 
+  # discrete assembly
+
   set.seed(1)
 
   config <- createStaticTestConfig(MIP = list(solver = "LPSYMPHONY"))
@@ -10,16 +12,46 @@ test_that("partitioning methods work", {
 
   n_partition <- 4
   o <- Split(config, constraints, n_partition = n_partition, partition_type = "test")
-  for (partition in o) {
-    subpool <- constraints@pool[partition]
+  for (partition in o@output) {
+    subpool <- constraints@pool[partition$i]
     expect_equal(subpool@ni, constraints@test_length)
   }
 
   n_partition <- 4
   o <- Split(config, constraints, n_partition = n_partition, partition_type = "pool")
-  for (partition in o) {
-    subpool <- constraints@pool[partition]
+  for (partition in o@output) {
+    subpool <- constraints@pool[partition$i]
     expect_equal(subpool@ni, constraints@pool@ni / n_partition)
+  }
+
+  n_partition <- 3
+  o <- Split(config, constraints, n_partition = n_partition, partition_type = "pool", partition_size_range = c(320, 340))
+  for (partition in o@output) {
+    subpool <- constraints@pool[partition$i]
+    expect_gte(subpool@ni, 320)
+    expect_lte(subpool@ni, 340)
+  }
+
+  # set-based assembly
+
+  set.seed(1)
+
+  config <- createStaticTestConfig(MIP = list(solver = "LPSYMPHONY", time_limit = 300))
+  constraints <- constraints_reading[1:6]
+
+  n_partition <- 3
+  o <- Split(config, constraints, n_partition = n_partition, partition_type = "test")
+  for (partition in o@output) {
+    subpool <- constraints@pool[partition$i]
+    expect_equal(subpool@ni, constraints@test_length)
+  }
+
+  n_partition <- 3
+  o <- Split(config, constraints, n_partition = n_partition, partition_type = "pool", partition_size_range = c(90, 110))
+  for (partition in o@output) {
+    subpool <- constraints@pool[partition$i]
+    expect_gte(subpool@ni,  90)
+    expect_lte(subpool@ni, 110)
   }
 
 })
