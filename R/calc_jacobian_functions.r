b245a2075f506f3a75ee60d59c5f4790394104ab
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2022-06-15 13:20:49 -0500

    use cpp functions in calcJacobian()

--- R/calc_jacobian_functions.r
+++ R/calc_jacobian_functions.r
@@ -3,11 +3,11 @@ NULL
 
 #' Calculate first derivative of log-likelihood
 #'
-#' \code{\link{calcJacobian}} is a function to calculate the first derivative of the log-likelihood function.
+#' \code{\link{calcJacobian}} is a function for calculating the first derivative of the log-likelihood function.
 #'
 #' @param object an \code{\link{item}} or an \code{\linkS4class{item_pool}} object.
 #' @param theta theta values to use.
-#' @param resp the response data to use.
+#' @param resp the response value to use for each item.
 #'
 #' @return
 #' \describe{
@@ -63,10 +63,8 @@ setMethod(
   f = "calcJacobian",
   signature = c("item_1PL", "numeric", "numeric"),
   definition = function(object, theta, resp) {
-    if (!(resp %in% c(0, 1))) {
-      return(NA)
-    }
-    return(resp - calcEscore(object, theta))
+    theta <- matrix(theta, , 1)
+    return(calcJacobian(object, theta, resp))
   }
 )
 
@@ -76,10 +74,8 @@ setMethod(
   f = "calcJacobian",
   signature = c("item_2PL", "numeric", "numeric"),
   definition = function(object, theta, resp) {
-    if (!(resp %in% c(0, 1))) {
-      return(NA)
-    }
-    return(object@slope * (resp - calcEscore(object, theta)))
+    theta <- matrix(theta, , 1)
+    return(calcJacobian(object, theta, resp))
   }
 )
 
@@ -89,11 +85,8 @@ setMethod(
   f = "calcJacobian",
   signature = c("item_3PL", "numeric", "numeric"),
   definition = function(object, theta, resp) {
-    if (!(resp %in% c(0, 1))) {
-      return(NA)
-    }
-    expected_score <- calcEscore(object, theta)
-    return(object@slope * (resp - expected_score) * (expected_score - object@guessing) / (expected_score * (1.0 - object@guessing)))
+    theta <- matrix(theta, , 1)
+    return(calcJacobian(object, theta, resp))
   }
 )
 
@@ -103,10 +96,8 @@ setMethod(
   f = "calcJacobian",
   signature = c("item_PC", "numeric", "numeric"),
   definition = function(object, theta, resp) {
-    if (!(resp %in% 0:(object@ncat - 1))) {
-      return(NA)
-    }
-    return(resp - calcEscore(object, theta))
+    theta <- matrix(theta, , 1)
+    return(calcJacobian(object, theta, resp))
   }
 )
 
@@ -116,10 +107,8 @@ setMethod(
   f = "calcJacobian",
   signature = c("item_GPC", "numeric", "numeric"),
   definition = function(object, theta, resp) {
-    if (!(resp %in% 0:(object@ncat - 1))) {
-      return(NA)
-    }
-    return(object@slope * (resp - calcEscore(object, theta)))
+    theta <- matrix(theta, , 1)
+    return(calcJacobian(object, theta, resp))
   }
 )
 
@@ -129,18 +118,86 @@ setMethod(
   f = "calcJacobian",
   signature = c("item_GR", "numeric", "numeric"),
   definition = function(object, theta, resp) {
+    theta <- matrix(theta, , 1)
+    return(calcJacobian(object, theta, resp))
+  }
+)
+
+#' @rdname calcJacobian-methods
+#' @aliases calcJacobian,item_1PL,matrix-method
+setMethod(
+  f = "calcJacobian",
+  signature = c("item_1PL", "matrix", "numeric"),
+  definition = function(object, theta, resp) {
+    if (!(resp %in% c(0, 1))) {
+      return(NA)
+    }
+    return(array_j_1pl(theta, object@difficulty, resp)[, 1])
+  }
+)
+
+#' @rdname calcJacobian-methods
+#' @aliases calcJacobian,item_2PL,matrix-method
+setMethod(
+  f = "calcJacobian",
+  signature = c("item_2PL", "matrix", "numeric"),
+  definition = function(object, theta, resp) {
+    if (!(resp %in% c(0, 1))) {
+      return(NA)
+    }
+    return(array_j_2pl(theta, object@slope, object@difficulty, resp)[, 1])
+  }
+)
+
+#' @rdname calcJacobian-methods
+#' @aliases calcJacobian,item_3PL,matrix-method
+setMethod(
+  f = "calcJacobian",
+  signature = c("item_3PL", "matrix", "numeric"),
+  definition = function(object, theta, resp) {
+    if (!(resp %in% c(0, 1))) {
+      return(NA)
+    }
+    return(array_j_3pl(theta, object@slope, object@difficulty, object@guessing, resp)[, 1])
+  }
+)
+
+#' @rdname calcJacobian-methods
+#' @aliases calcJacobian,item_PC,matrix-method
+setMethod(
+  f = "calcJacobian",
+  signature = c("item_PC", "matrix", "numeric"),
+  definition = function(object, theta, resp) {
     if (!(resp %in% 0:(object@ncat - 1))) {
       return(NA)
     }
-    resp <- resp + 1
-    prob <- calcProb(object, theta)
-    ps <- matrix(NA, length(theta), object@ncat + 1)
-    ps[, 1] <- 1
-    ps[, object@ncat + 1] <- 0
-    for (k in 2:(object@ncat)) {
-      ps[, k] <- ps[, k - 1] - prob[, k - 1]
+    return(array_j_pc(theta, object@threshold, resp)[, 1])
+  }
+)
+
+#' @rdname calcJacobian-methods
+#' @aliases calcJacobian,item_GPC,matrix-method
+setMethod(
+  f = "calcJacobian",
+  signature = c("item_GPC", "matrix", "numeric"),
+  definition = function(object, theta, resp) {
+    if (!(resp %in% 0:(object@ncat - 1))) {
+      return(NA)
     }
-    return(object@slope * ((ps[, resp] * (1 - ps[, resp]) - ps[, resp + 1] * (1 - ps[, resp + 1])) / prob[, resp]))
+    return(array_j_gpc(theta, object@slope, object@threshold, resp)[, 1])
+  }
+)
+
+#' @rdname calcJacobian-methods
+#' @aliases calcJacobian,item_GR,matrix-method
+setMethod(
+  f = "calcJacobian",
+  signature = c("item_GR", "matrix", "numeric"),
+  definition = function(object, theta, resp) {
+    if (!(resp %in% 0:(object@ncat - 1))) {
+      return(NA)
+    }
+    return(array_j_gr(theta, object@slope, object@category, resp)[, 1])
   }
 )
 
