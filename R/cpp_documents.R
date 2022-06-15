045fdc1c1ca4778b617fb3103f66daafb9ff46db
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2022-06-15 13:14:04 -0500

    add documentation for cpp jacobian functions

--- R/cpp_documents.R
+++ R/cpp_documents.R
@@ -289,6 +289,57 @@ NULL
 #' @name calc_likelihood
 NULL
 
+#' (C++) Calculate first derivative of log-likelihood
+#'
+#' \code{j_*()} and \code{array_j_*()} are C++ functions for calculating the first derivative of the log-likelihood function.
+#'
+#' \code{j_*()} functions accept a single theta value, and \code{array_j_*()} functions accept multiple theta values.
+#'
+#' Currently supports unidimensional models.
+#'
+#' \itemize{
+#'   \item{\code{j_1pl()}, \code{array_j_1pl()}}: 1PL models
+#'   \item{\code{j_2pl()}, \code{array_j_2pl()}}: 2PL models
+#'   \item{\code{j_3pl()}, \code{array_j_3pl()}}: 3PL models
+#'   \item{\code{j_pc()}, \code{array_j_pc()}}: PC (partial credit) models
+#'   \item{\code{j_gpc()}, \code{array_j_gpc()}}: GPC (generalized partial credit) models
+#'   \item{\code{j_gr()}, \code{array_j_gr()}}: GR (graded response) models
+#' }
+#'
+#' @param x the theta value. This must be a column vector in matrix form for \code{array_j_*()} functions.
+#' @param a,b,c the item parameters.
+#' @param u the response value.
+#'
+#' @examples
+#' u <- 1
+#'
+#' x <- 0.5
+#' j_1pl(x, 1, u)
+#' j_2pl(x, 1, 2, u)
+#' j_3pl(x, 1, 2, 0.25, u)
+#' j_pc(x, c(0, 1), u)
+#' j_gpc(x, 2, c(0, 1), u)
+#' j_gr(x, 2, c(0, 2), u)
+#'
+#' x <- matrix(seq(-3, 3, 1)) # column vector in matrix form
+#' array_j_1pl(x, 1, u)
+#' array_j_2pl(x, 1, 2, u)
+#' array_j_3pl(x, 1, 2, 0.25, u)
+#' array_j_pc(x, c(0, 1), u)
+#' array_j_gpc(x, 2, c(0, 1), u)
+#' array_j_gr(x, 2, c(0, 2), u)
+#'
+#' @template 1pl-ref
+#' @template 2pl-ref
+#' @template 3pl-ref
+#' @template pc-ref
+#' @template gpc-ref
+#' @template gr-ref
+#'
+#' @name j_item
+#' @order 0
+NULL
+
 #' Calculate theta estimates using EB (Empirical Bayes) method
 #'
 #' \code{theta_EB_single} and \code{theta_EB} are functions to calculate theta estimates using EB (Empirical Bayes) method.
