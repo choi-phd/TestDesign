e97a077998dd04ebffcc8e01b6ab17c808b07317
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2022-07-26 20:10:02 -0500

    move shadowtest functions to shadowtest_functions.r

--- R/shadowtest_functions.R
+++ R/shadowtest_functions.R
@@ -2,6 +2,39 @@
 NULL
 
 #' @noRd
+initializeShadowEngine <- function(constants, refresh_policy) {
+
+  refresh_method   <- toupper(refresh_policy$method)
+  refresh_position <- refresh_policy$position
+  refresh_interval <- refresh_policy$interval
+  test_length      <- constants$test_length
+
+  refresh_shadow    <- rep(FALSE, test_length)
+  refresh_shadow[1] <- TRUE
+  if (refresh_method %in% c("ALWAYS", "THRESHOLD")) {
+    refresh_shadow[1:test_length] <- TRUE
+  }
+  if (refresh_method %in% c("POSITION")) {
+    if (!all(refresh_position %in% 1:test_length)) {
+      stop("config@refresh_policy: $position must be within test length")
+    }
+    refresh_shadow[refresh_position] <- TRUE
+  }
+  if (refresh_method %in% c("INTERVAL", "INTERVAL-THRESHOLD")) {
+    if (!(refresh_interval >= 1 && refresh_interval <= test_length)) {
+      stop("config@refresh_policy: $interval must be at least 1 and not greater than test length")
+    }
+    refresh_shadow[seq(1, test_length, refresh_interval)] <- TRUE
+  }
+  if (constants$set_based_refresh && !constants$set_based) {
+    stop(sprintf("config@refresh_policy: stimulus-based constraint is required for $method '%s'", refresh_method))
+  }
+
+  return(refresh_shadow)
+
+}
+
+#' @noRd
 assembleShadowTest <- function(
   j, position, o,
   eligible_flag,
@@ -77,4 +110,102 @@ assembleShadowTest <- function(
 #' @noRd
 isShadowtestOptimal <- function(shadowtest) {
   return(isOptimal(shadowtest$status, shadowtest$solver))
+}
+
+#' @noRd
+selectItemFromShadowTest <- function(shadow_test, position, constants, x) {
+
+  o <- list()
+
+  o$n_remaining <- constants$test_length - position
+  o$is_last_item_in_this_set <- FALSE
+  o$new_stimulus_selected    <- FALSE
+
+  o$stimulus_of_previous_item <- 0
+
+  remaining <- which(!shadow_test[["INDEX"]] %in% x@administered_item_index[0:(position - 1)])
+
+  if (position == 1 & !constants$set_based) {
+    idx                 <- remaining[1]
+    o$item_selected     <- shadow_test[["INDEX"]][idx]
+    o$stimulus_selected <- NA
+  }
+  if (position == 1 & constants$set_based) {
+    idx                 <- remaining[1]
+    o$item_selected     <- shadow_test[["INDEX"]][idx]
+    o$stimulus_selected <- shadow_test[["STINDEX"]][idx]
+  }
+  if (position > 1 & !constants$set_based) {
+    idx                 <- remaining[1]
+    o$item_selected     <- shadow_test[["INDEX"]][idx]
+    o$stimulus_selected <- NA
+  }
+  if (position > 1 & constants$set_based) {
+    o$stimulus_of_previous_item <- x@administered_stimulus_index[position - 1]
+    if (!is.na(o$stimulus_of_previous_item)) {
+      remaining_items_within_stimulus <- shadow_test[["STINDEX"]][remaining] == o$stimulus_of_previous_item
+      if (any(remaining_items_within_stimulus, na.rm = TRUE)) {
+        idx <- remaining[which(remaining_items_within_stimulus)][1]
+      } else {
+        idx <- remaining[1]
+      }
+    }
+    if (is.na(o$stimulus_of_previous_item)) {
+      idx <- remaining[1]
+    }
+    o$item_selected     <- shadow_test[["INDEX"]][idx]
+    o$stimulus_selected <- shadow_test[["STINDEX"]][idx]
+  }
+
+
+  if (is.na(
+    o$stimulus_of_previous_item != o$stimulus_selected) |
+    o$stimulus_of_previous_item != o$stimulus_selected) {
+    o$new_stimulus_selected <- TRUE
+  }
+
+  if (sum(shadow_test[["STINDEX"]][remaining] == o$stimulus_selected, na.rm = TRUE) == 1) {
+    o$is_last_item_in_this_set <- TRUE
+  }
+  if (is.na(o$stimulus_selected)) {
+    o$is_last_item_in_this_set <- TRUE
+  }
+  if (o$n_remaining == 0) {
+    o$is_last_item_in_this_set <- TRUE
+  }
+
+  return(o)
+
+}
+
+#' @noRd
+shouldShadowBeRefreshed <- function(position, refresh_policy, refresh_shadow, theta_change, constants, stimulus_record) {
+
+  refresh_method <- toupper(refresh_policy$method)
+
+  if (position == 1) {
+    return(TRUE)
+  }
+  if (refresh_method == "ALWAYS") {
+    return(TRUE)
+  }
+  if (refresh_method %in% c("POSITION", "INTERVAL") && refresh_shadow[position]) {
+    return(TRUE)
+  }
+  if (refresh_method == "THRESHOLD") {
+    if (abs(theta_change) > refresh_policy$threshold) {
+      return(TRUE)
+    }
+  }
+  if (refresh_method == "INTERVAL-THRESHOLD") {
+    if (abs(theta_change) > refresh_policy$threshold && refresh_shadow[position]) {
+      return(TRUE)
+    }
+  }
+  if (constants$set_based_refresh && constants$set_based && stimulus_record$just_finished_this_set) {
+    return(TRUE)
+  }
+
+  return(FALSE)
+
 }
