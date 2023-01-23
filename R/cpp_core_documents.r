e1e7cc02ce3cdcf06a34e908aea6e9221b4b0b88
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2023-01-23 12:45:21 -0600

    add cpp multidimensional expected score functions

--- R/cpp_core_documents.r
+++ R/cpp_core_documents.r
@@ -69,7 +69,7 @@ NULL
 #' }
 #'
 #' @param x the theta value. This must be a column vector in matrix form for \code{array_e_*()} functions.
-#' @param a,b,c the item parameters.
+#' @param a,b,c,d the item parameters.
 #'
 #' @examples
 #' x <- 0.5
