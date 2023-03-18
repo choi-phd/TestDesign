2012c9150e6c7169f9d23b2c1f0f4c452f8a14cf
Seung W. Choi <44212658+choi-phd@users.noreply.github.com>
2023-03-18 15:43:46 -0500

    update diagram

--- figures/diagram.R
+++ figures/diagram.R
@@ -8,7 +8,7 @@ names <- c("Item pool\nloadItemPool()",
            "Optimal Test Assembly",
            "Adaptive Assembly\nShadow()",
            "Maximum information\nTarget TIF\nTarget TCC",
-           "",
+           "Multiple-form Assembly\nSplit()",
            "Linear-on-the-fly\nMST on the fly\nFully adaptive\nHybrid")
 
 
@@ -28,6 +28,7 @@ M[5, 6] = ""
 M[7, 6] = ""
 
 M[8, 5] = ""
+M[9, 6] = ""
 M[10, 7] = ""
 
 arr = matrix(nrow = k, ncol = k, byrow = TRUE, data = .25)
@@ -38,18 +39,18 @@ coord = coordinates(pos = c(1, 2, 1, 3, 3))
 coord[2, 1] = 0.5
 
 setEPS()
-postscript("figures/diagram.eps", width = 12, height = 8, family = "Courier")
+postscript("figures/diagram.eps", width = 12, height = 8, family = "Helvetica")
 
 plotmat(M, coord, name = names, lwd = 1, curve = 0,
         box.lwd = c(2, 2, 2, 2, 2, 2, 2, 2, 2, 2), cex.txt = 0.8,
         box.size = c(0.1, 0.1, 0.1, 0.1, 0.1, 0.125, 0.1),
         box.type = 'rect',
-        box.prop = c(0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.5, 0.5, 0.5),
+        box.prop = c(0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.5, 0.3, 0.5),
         box.col = c('white', 'white', 'white', 'white', 'white', 'grey75', 'white', 'white', 'white', 'white'),
-        box.lcol = c('black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'white', 'black'),
+        box.lcol = c('black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black'),
         txt.col = c('black', 'black', 'black', 'black', 'black', 'black', 'black'),
         shadow.col = c('grey75', 'grey75', 'grey75', 'grey75', 'grey75', 'grey50', 'grey75'),
-        shadow.size = c(0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0, 0.005),
+        shadow.size = c(0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005),
         arr.length = arr,
         arr.width = arr, arr.type = "triangle", arr.pos = .5,
         font = 2)
